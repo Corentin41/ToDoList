3392f93914330a9d68e321a79f70bec62332eaf8
Foguu <118466822+Foguu@users.noreply.github.com>
2024-03-10 14:09:33 +0100

    Utilisation de la modalBottomSheet

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,26 +12,45 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 20,
-        ),
-        child: Column(
-          children: [
-            Expanded(
-                child: ListView(
-                  children: [
-                    Container(
-                      child: const Text('Tâches',),
-                    ),
-                    for (ToDo todo in todosList)
-                      TodoItem(todo: todo,),
-                  ],
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 20,
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                    child: ListView(
+                      children: [
+                        Container(
+                          child: const Text('Tâches',),
+                        ),
+                        for (ToDo todo in todosList)
+                          TodoItem(todo: todo,),
+                      ],
+                    )
                 )
-            )
-          ],
-        ),
+              ],
+            ),
+          ),
+          ElevatedButton(onPressed: (){
+            showModalBottomSheet(context: context, builder: (BuildContext context){
+              return SizedBox(
+                height: 400,
+                child: Center(
+                  child: ElevatedButton(
+                    child: const Text('close'),
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              );
+            });
+          }, child: const Text('data'))
+        ],
       ),
     );
   }
